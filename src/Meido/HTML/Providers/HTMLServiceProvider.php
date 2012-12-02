5c38a756072192296680ac3d1ab9f0a9600afcc7
Joni Chandra <joniliciously@gmail.com>
2012-12-02 11:44:46 +0700

    updated to work with latest changes in illuminate/foundation

--- src/Meido/HTML/Providers/HTMLServiceProvider.php
+++ src/Meido/HTML/Providers/HTMLServiceProvider.php
@@ -6,6 +6,13 @@ use Meido\HTML\HTML;
 class HTMLServiceProvider extends ServiceProvider {
 
 	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
 	 * Register the service provider.
 	 *
 	 * @return void
@@ -16,6 +23,16 @@ class HTMLServiceProvider extends ServiceProvider {
 		{
 			return new HTML($app);
 		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('html');
 	}
 
 }
